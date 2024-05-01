c87ed08f30fcfa9e1bdcf0a349561ca8be066417
Caceresenzo <caceresenzo1502@gmail.com>
2024-05-01 15:12:30 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,17 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, _ = server_socket.accept()
-    client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    io = socket.SocketIO(client, "rw")
+
+    request_line = io.readline()[:-2].decode("ascii")
+    parts = request_line.split(" ")
+    method, path, version = parts
+
+    if path == "/":
+        client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
     client.close()
 
 if __name__ == "__main__":
