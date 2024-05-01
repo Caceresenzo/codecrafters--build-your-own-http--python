114a011a68a87a104d2cbe50a1e0b494d5ec538c
Caceresenzo <caceresenzo1502@gmail.com>
2024-05-01 15:07:50 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ def main():
     print("codecrafters build-your-own-http")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
-
+    client, _ = server_socket.accept()
+    client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    client.close()
 
 if __name__ == "__main__":
     main()
