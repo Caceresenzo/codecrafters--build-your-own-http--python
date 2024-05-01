1778db5179647e5b3b1f5ad33db513a64c97256e
Caceresenzo <caceresenzo1502@gmail.com>
2024-05-01 15:23:51 +0200

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -1,42 +1,52 @@
+import os
 import socket
 
 
 def main():
     print("codecrafters build-your-own-http")
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client, _ = server_socket.accept()
-    io = socket.SocketIO(client, "rw")
+    while True:
+        server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+        client, addr = server_socket.accept()
 
-    request_line = io.readline()[:-2].decode("ascii")
-    parts = request_line.split(" ")
-    method, path, version = parts
+        print("connected", addr)
 
-    headers = {}
-    while line := io.readline()[:-2].decode("ascii"):
-        key, value = line.split(": ")
-        headers[key.lower()] = value
+        pid = os.fork()
+        if pid != 0:
+            continue
 
-    if path == "/":
-        client.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    elif path.startswith("/echo/"):
-        message = path[6:]
-        client.send(b"HTTP/1.1 200 OK\r\n")
-        client.send(b"Content-Type: text/plain\r\n")
-        client.send(f"Content-Length: {len(message)}\r\n".encode("ascii"))
-        client.send(b"\r\n")
-        client.send(message.encode("ascii"))
-    elif path == "/user-agent":
-        message = headers["User-Agent".lower()]
-        client.send(b"HTTP/1.1 200 OK\r\n")
-        client.send(b"Content-Type: text/plain\r\n")
-        client.send(f"Content-Length: {len(message)}\r\n".encode("ascii"))
-        client.send(b"\r\n")
-        client.send(message.encode("ascii"))
-    else:
-        client.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        io = socket.SocketIO(client, "rw")
 
-    client.close()
+        request_line = io.readline()[:-2].decode("ascii")
+        parts = request_line.split(" ")
+        method, path, version = parts
+
+        headers = {}
+        while line := io.readline()[:-2].decode("ascii"):
+            key, value = line.split(": ")
+            headers[key.lower()] = value
+
+        if path == "/":
+            client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        elif path.startswith("/echo/"):
+            message = path[6:]
+            client.send(b"HTTP/1.1 200 OK\r\n")
+            client.send(b"Content-Type: text/plain\r\n")
+            client.send(f"Content-Length: {len(message)}\r\n".encode("ascii"))
+            client.send(b"\r\n")
+            client.send(message.encode("ascii"))
+        elif path == "/user-agent":
+            message = headers["User-Agent".lower()]
+            client.send(b"HTTP/1.1 200 OK\r\n")
+            client.send(b"Content-Type: text/plain\r\n")
+            client.send(f"Content-Length: {len(message)}\r\n".encode("ascii"))
+            client.send(b"\r\n")
+            client.send(message.encode("ascii"))
+        else:
+            client.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+        client.close()
+        exit(0)
 
 if __name__ == "__main__":
     main()
